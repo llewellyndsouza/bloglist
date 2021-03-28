7ccb42cf669bc5f793a3d91c3512e8c800e3e646
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-03-28 16:31:38 +0530

    CRUD established

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,13 +2,13 @@ const logger = require("../utils/logger");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", async (request, response, next) => {
+blogsRouter.get("/", async (request, response) => {
   logger.info("Get all the blogs");
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
-blogsRouter.get("/:id", async (request, response, next) => {
+blogsRouter.get("/:id", async (request, response) => {
   logger.info("Get particular blog with id:", request.params.id);
   const blog = await Blog.findById(request.params.id);
   response.json(blog);
@@ -16,10 +16,43 @@ blogsRouter.get("/:id", async (request, response, next) => {
 
 blogsRouter.post("/", async (request, response, next) => {
   logger.info("Add new blog", request.body);
-  const blog = new Blog(request.body);
+  const body = request.body;
 
+  if (!body.title || !body.url) {
+    console.log("400 Bad request - url or title missing");
+    response.status(400).end();
+    return;
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  });
   const savedBlog = await blog.save();
   response.json(savedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  logger.info("Deleting a blog with id:", request.params.id);
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  logger.info("Updating blog with id:", request.params.id);
+  const body = request.body;
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  };
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -23,9 +23,43 @@ describe("blog api", () => {
       .expect("Content-Type", /application\/json/);
   });
 
-  test("returned object has id", async () => {
+  test("returned object has 'id'", async () => {
     const response = await helper.blogsInDb();
     expect(response[0].id).toBeDefined();
+  });
+
+  test("new blog is created in db", async () => {
+    let blogObj = new Blog({
+      title: "TEST new String11",
+      author: "Another new String22",
+      url: "http://fb.com",
+      likes: 420,
+    });
+    let newBlog = await blogObj.save();
+
+    let response = await helper.blogsInDb();
+    expect(response.length).toBe(helper.initBlogs.length + 1);
+    expect(response[response.length - 1].title).toBe(blogObj.title);
+  });
+
+  test("likes default to 0 when missing", async () => {
+    let blogObj = new Blog({
+      title: "TEST default String11",
+      author: "Another default String22",
+      url: "http://maps.com",
+    });
+    let newBlog = await blogObj.save();
+
+    let response = await helper.blogsInDb();
+    expect(response[response.length - 1].likes).toBe(0);
+  });
+
+  test("check for missing title or url", async () => {
+    let blogObj = new Blog({
+      author: "Another default String22",
+      url: "http://maps.com",
+    });
+    let response = await api.post("/api/blogs").send(blogObj).expect(400);
   });
 
   // test("there are 2 notes", async () => {
